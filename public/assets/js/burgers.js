e09d7e7dae51c9b4a5b41d47d5ee31f55cb2acec
mwturner611 <mwturner611@gmail.com>
2020-05-06 17:29:10 -0500

    JQuery buttons and ajax calls

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,4 +1,25 @@
 $(function() {
+    $(".change-devour").on("click", function(event){
+        var id = $(this).data("id");
+        var newDevoured = $(this).data("newDevoured");
+
+        var newEatState = {
+            devoured: newDevoured
+        }
+
+        $.ajax("/api/burger/" + id, {
+            type: "PUT",
+            data: newEatState
+        }).then(
+            function() {
+                location.reload();
+            }
+        )
+    })
+
+
+
+
     $(".create-form").on("submit", function(event) {
         // Make sure to preventDefault on a submit event.
         event.preventDefault();
